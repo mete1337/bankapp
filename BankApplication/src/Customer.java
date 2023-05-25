765cb729c8c2464f1fed1545750662fefaa8a66a
mete1337 <akdenizmete@icloud.com>
2023-05-25 22:36:32 +0300

    customer class

---
+++ BankApplication/src/Customer.java
@@ -0,0 +1,32 @@
+
+public class Customer  {
+
+	protected int id;
+	protected String password;
+	protected Account[] accounts;
+ 	
+	
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Account[] getAccount() {
+		return accounts;
+	}
+	
+	
+	
+}
