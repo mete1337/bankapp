883b473eeb92e6783970f7dc10a386b4044cf9e6
ulasoncull <ulasoncull@hotmail.com>
2023-05-25 20:16:02 +0300

    a

--- BankApplication/src/IdAndPasswords.java
+++ BankApplication/src/IdAndPasswords.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
 
 public class IdAndPasswords {
-
+	HashMap<String, String> loginInfo = new HashMap<String, String>();
+	IdAndPasswords(){
+		loginInfo.put("Mete", "anan");
+		loginInfo.put("Ulaþ", "anan");
+	}
+	protected HashMap getLoginInfo(){
+		return loginInfo;
+	}
 }
