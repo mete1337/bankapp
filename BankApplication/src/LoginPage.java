e93dd87726ae5b7806e5ae00b7d5b0061282e94d
ulasoncull <ulasoncull@hotmail.com>
2023-05-25 21:11:04 +0300

    a

--- BankApplication/src/LoginPage.java
+++ BankApplication/src/LoginPage.java
@@ -1,9 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.HashMap;
 
-public class LoginPage {
+public class LoginPage implements ActionListener {
 	HashMap<String, String> loginInfo = new HashMap<String,String>();
 	
 	LoginPage(HashMap<String, String> loginInfoOriginal){
 		loginInfo = loginInfoOriginal;
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
