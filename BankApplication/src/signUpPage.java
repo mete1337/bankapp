8b18fe716d4310c77dae0ac6f6007abaedc33a85
ulasoncull <ulasoncull@hotmail.com>
2023-05-26 00:29:42 +0300

    a

--- BankApplication/src/signUpPage.java
+++ BankApplication/src/signUpPage.java
@@ -1,4 +1,3 @@
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,20 +8,20 @@ import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class LoginPage implements ActionListener {
+public class signUpPage implements ActionListener {
 	private JFrame frame = new JFrame();
-	private JButton loginButton = new JButton("Login");
 	private JButton resetButton = new JButton("Reset");
 	private JButton signupButton = new JButton("SignUp");
 	private JTextField userIDField = new JTextField();
 	private JPasswordField userPasswordField = new JPasswordField();
 	private JLabel userIDLabel = new JLabel("userID:");
+	public HashMap<String, String> getLoginInfo() {
+		return loginInfo;
+	}
 	private JLabel userPasswordLabel = new JLabel("password:");
 	private JLabel messageLabel = new JLabel();
-	
 	HashMap<String, String> loginInfo = new HashMap<String,String>();
-	
-	LoginPage(HashMap<String, String> loginInfoOriginal){
+	public signUpPage(HashMap<String, String> loginInfoOriginal) {
 		loginInfo = loginInfoOriginal;
 		userIDLabel.setBounds(50,100,75,25);
 		userPasswordLabel.setBounds(50,150,75,25);
@@ -30,12 +29,9 @@ public class LoginPage implements ActionListener {
 		messageLabel.setFont(new Font(null,Font.ITALIC,25));
 		userIDField.setBounds(125, 100, 200, 25);
 		userPasswordField.setBounds(125, 150, 200, 25);
-		signupButton.setBounds(25, 200, 100, 25);
+		signupButton.setBounds(125, 200, 100, 25);
 		signupButton.setFocusable(false);
 		signupButton.addActionListener(this);
-		loginButton.setBounds(125, 200, 100, 25);
-		loginButton.setFocusable(false);
-		loginButton.addActionListener(this);
 		resetButton.setBounds(225, 200, 100, 25);
 		resetButton.setFocusable(false);
 		resetButton.addActionListener(this);
@@ -44,7 +40,6 @@ public class LoginPage implements ActionListener {
 		frame.add(messageLabel);
 		frame.add(userIDField);
 		frame.add(userPasswordField);
-		frame.add(loginButton);
 		frame.add(resetButton);
 		frame.add(signupButton);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,39 +47,22 @@ public class LoginPage implements ActionListener {
 		frame.setLayout(null);
 		frame.setVisible(true);
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if(e.getSource()==signupButton) {
+			String userID = userIDField.getText();
+			String password = String.valueOf(userPasswordField.getPassword());
+			loginInfo.put(userID, password);
+			messageLabel.setText("U are registered!");
+			frame.dispose();
+			
+		}
 		if(e.getSource()==resetButton) {
 			userIDField.setText("");
 			userPasswordField.setText("");
 		}
-		if(e.getSource()==loginButton) {
-			String userID = userIDField.getText();
-			String password = String.valueOf(userPasswordField.getPassword());
 			
-			if(loginInfo.containsKey(userID)&&loginInfo.get(userID).equals(password)) {
-				messageLabel.setForeground(Color.green);
-				messageLabel.setText("Login Succesful");
-				frame.dispose();
-				WelcomePage welcomePage = new WelcomePage();
-				
-				
-			}
-			else {
-				messageLabel.setForeground(Color.red);
-				messageLabel.setText("ID or Password is not invalid");
-			}
-				
-				
-		}
-		if(e.getSource()==signupButton) {
-			signUpPage signuppage = new signUpPage(this.loginInfo);
-			loginInfo = signuppage.getLoginInfo();
-			
-		}
-		
 	}
+}	
 	
-}
