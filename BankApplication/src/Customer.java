39a52667d33119cf44e38273da4cf2beb44f22f0
mete1337 <akdenizmete@icloud.com>
2023-05-27 21:34:50 +0300

    customer class

--- BankApplication/src/Customer.java
+++ BankApplication/src/Customer.java
@@ -1,35 +1,58 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+
 
 public class Customer{
 
 	protected String id;
 	protected String password;
-	protected DepositAccount depositacc;
-	protected Account acc;
-	protected CurrencyAccount currencyacc;
+	protected Account[] accounts = new Account[3];
+
+    public Customer(String id, String password) {
+       this.id = id;
+       this.password = password;
+       this.accounts = new Account[3];
+    }
+    
+   
+
+    public void transferMoney(Customer receiverCustomer, double amount) {
+        if (receiverCustomer.accounts[0] == null && this.accounts[0] == null) {
+        	System.out.println("there is no account");
+        }
+        else {
+        	double senderBalance = this.accounts[0].getBalance();
+        	if(senderBalance > amount) {
+        	this.accounts[0].setBalance(senderBalance - amount);
+        	double receiverBalance = receiverCustomer.accounts[0].getBalance();
+        	receiverCustomer.accounts[0].setBalance(receiverBalance + amount);
+        	}
+        	else {
+        		System.out.println("Balance is not enough for transfer");
+        	}
+        }
+    }
+    
 	
 	
-	Customer(String id, String password){
+	public void createSavingAccount() {
+	
 		
-		this.id = id;
-		this.password = password;
-	}
-	
-	
-	
-	public void createDepositAccount() {
-	
+		SavingAccount saveacc = new SavingAccount(this.id, 0);
+		this.accounts[1] = saveacc;
+		
 	}
 	
 	public void createAccount() {
 		
-		Account acc = new Account();
+		Account acc = new Account(this.id, 0);
+		this.accounts[0] = acc;
 	}
 	
-	public void createCurrency() {
+	public void createCurrency(String dovizCinsi) {
 		
-		CurrencyAccount currencyacc = new CurrencyAccount();
+		CurrencyAccount currencyacc = new CurrencyAccount(this.id, 0, dovizCinsi);
+		this.accounts[2] = currencyacc;
+		
 	}
 	
 	public String getId() {
